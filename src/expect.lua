b60cbb2e9b80ebce7940ad79ed133cf54ad700e5
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-08-22 14:16:28 +0900

    feat: add assertion to validate received data to be expectation object

--- src/expect.lua
+++ src/expect.lua
@@ -5,6 +5,33 @@
 ---@overload fun(value: any): Expectation
 local expect = {}
 
+---@param value any
+---@return boolean
+local function isExpectation(value)
+    if type(value) ~= "table" then
+        return false
+    end
+
+    local meta = getmetatable(value)
+    while meta ~= nil do
+        if meta.__index == expect then
+            return true
+        end
+
+        meta = getmetatable(meta.__index)
+    end
+
+    return false
+end
+
+---@param value any
+local function assertExpectation(value)
+    assert(
+        isExpectation(value),
+        "Received value seems not to be Expectation.\nPerhaps you are using \".\" instead of \":\" to call the method?"
+    )
+end
+
 ---@private
 ---@param pass boolean
 ---@param message string
@@ -20,6 +47,8 @@ end
 ---Expects `==` equality.
 ---@param expected any
 function expect.toBe(self, expected)
+    assertExpectation(self)
+
     self:assert(
         self.value == expected,
         string.format(
@@ -39,6 +68,8 @@ end
 ---@param number number
 ---@param numDigits integer?
 function expect.toBeCloseTo(self, number, numDigits)
+    assertExpectation(self)
+
     if numDigits == nil then
         numDigits = 2
     end
@@ -72,6 +103,8 @@ function expect.toBeCloseTo(self, number, numDigits)
 end
 
 function expect.toBeFalsy(self)
+    assertExpectation(self)
+
     self:assert(
         not self.value,
         string.format("expect(received):toBeFalsy()\nReceived: %s", tostring(self.value)),
@@ -81,6 +114,8 @@ end
 
 ---@param another any
 function expect.toBeGraterThan(self, another)
+    assertExpectation(self)
+
     self:assert(
         self.value > another,
         string.format(
@@ -98,6 +133,8 @@ end
 
 ---@param another any
 function expect.toBeGraterThanOrEqual(self, another)
+    assertExpectation(self)
+
     self:assert(
         self.value >= another,
         string.format(
@@ -115,6 +152,8 @@ end
 
 ---@param another any
 function expect.toBeLessThan(self, another)
+    assertExpectation(self)
+
     self:assert(
         self.value < another,
         string.format(
@@ -132,6 +171,8 @@ end
 
 ---@param another any
 function expect.toBeLessThanOrEqual(self, another)
+    assertExpectation(self)
+
     self:assert(
         self.value <= another,
         string.format(

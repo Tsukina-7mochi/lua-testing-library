f5323b72691ac0ccb47293b419dcfcb519f81017
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-08-22 13:21:11 +0900

    feat: implement expect.toBeLessThanOrEqual

---
+++ test/expect/toBeLessThanOrEqual.lua
@@ -0,0 +1,21 @@
+local describe = require("src.test").describe
+local test = require("src.test").test
+local expect = require("src.expect")
+
+describe("toBeLessThanOrEqual", function()
+    test("0 is less than 1", function()
+        expect(0):toBeLessThanOrEqual(1)
+    end)
+
+    test("0 is equals to 0", function()
+        expect(0):toBeLessThanOrEqual(0)
+    end)
+
+    test("1 is not less or equals to than 0", function()
+        expect(1).not_:toBeLessThanOrEqual(0)
+    end)
+
+    test("\"aaa\" is less than \"aab\"", function()
+        expect("aaa"):toBeLessThanOrEqual("aab")
+    end)
+end)

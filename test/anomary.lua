d46b487d64b024d9e254232d376d6e6cf6a3ea73
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-08-25 16:31:03 +0900

    feat: add test that failes to see error messages

---
+++ test/anomary.lua
@@ -0,0 +1,89 @@
+local describe = require("src.test").describe
+local test = require("src.test").test
+local expect = require("src.expect")
+
+describe("anomary", function()
+    test("not an expectation", function()
+        expect(0).toBe()
+    end)
+
+    test("expect.toBe", function()
+        expect(0):toBe(1)
+    end)
+
+    test("expect.not_.toBe", function()
+        expect(0).not_:toBe(0)
+    end)
+
+    test("expect.toBeCloseTo", function()
+        expect(0):toBeCloseTo(1)
+    end)
+
+    test("expect.not_.toBeCloseTo", function()
+        expect(0).not_:toBeCloseTo(1)
+    end)
+
+    test("expect.toBeTruthy", function()
+        expect(false):toBeTruthy()
+    end)
+
+    test("expect.not.toBeTruthy", function()
+        expect(true).not_:toBeTruthy()
+    end)
+
+    test("expect.toBeFalsy", function()
+        expect(true):toBeFalsy()
+    end)
+
+    test("expect.not.toBeFalsy", function()
+        expect(false).not_:toBeFalsy()
+    end)
+
+    test("expect.toBeGraterThan", function()
+        expect(0):toBeGraterThan(1)
+    end)
+
+    test("expect.not.toBeGraterThan", function()
+        expect(1).not_:toBeGraterThan(0)
+    end)
+
+    test("expect.toBeGraterThanOrEqual", function()
+        expect(0):toBeGraterThanOrEqual(1)
+    end)
+
+    test("expect.not.toBeGraterThanOrEqual", function()
+        expect(1).not_:toBeGraterThanOrEqual(0)
+    end)
+
+    test("expect.toBeLessThan", function()
+        expect(1):toBeLessThan(0)
+    end)
+
+    test("expect.not.toBeLessThan", function()
+        expect(0).not_:toBeLessThan(1)
+    end)
+
+    test("expect.toBeLessThanOrEqual", function()
+        expect(1):toBeLessThanOrEqual(0)
+    end)
+
+    test("expect.not.toBeLessThanOrEqual", function()
+        expect(0).not_:toBeLessThanOrEqual(1)
+    end)
+
+    test("expect.toBeNil", function()
+        expect(0):toBeNil()
+    end)
+
+    test("expect.not.toBeNil", function()
+        expect(nil).not_:toBeNil()
+    end)
+
+    test("expect.toContain", function()
+        expect({ 1, 2, 3 }):toContain(0)
+    end)
+
+    test("expect.not.toContain", function()
+        expect({ 1, 2, 3 }).not_:toContain(1)
+    end)
+end)
